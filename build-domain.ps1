cb1aed0d0da214765e7df50bea7e1fb9f4fb59ff
Allan hardy <al_hardy@live.com.au>
2016-05-11 11:14:59 +1000

    updated domain build script and make ClientAuthenticaionDetails public so we can use this in domain auth for auditing

--- build-domain.ps1
+++ build-domain.ps1
@@ -1,5 +1,4 @@
 Param(
-	[string]$buildNumber = "0",
 	[string]$preRelease = $null
 )
 
@@ -14,15 +13,15 @@ gci .\source -Recurse "packages.config" |% {
 
 Import-Module .\source\packages\psake.4.4.1\tools\psake.psm1
 
-if(Test-Path Env:\APPVEYOR_BUILD_NUMBER){
-	$buildNumber = [int]$Env:APPVEYOR_BUILD_NUMBER
-	$task = "appVeyor"
+#if(Test-Path Env:\APPVEYOR_BUILD_NUMBER){
+#	$buildNumber = [int]$Env:APPVEYOR_BUILD_NUMBER
+#	$task = "appVeyor"
     
-    Write-Host "Using APPVEYOR_BUILD_NUMBER"
-}
+#    Write-Host "Using APPVEYOR_BUILD_NUMBER"
+#}
 
-"Build number $buildNumber"
+#"Build number $buildNumber"
 
-Invoke-Psake .\default-domain.ps1 $task -framework "4.0x64" -properties @{ buildNumber=$buildNumber; preRelease=$preRelease }
+Invoke-Psake .\default-domain.ps1 $task -framework "4.0x64" -properties @{ buildNumber=0; preRelease=$preRelease }
 
 Remove-Module psake
