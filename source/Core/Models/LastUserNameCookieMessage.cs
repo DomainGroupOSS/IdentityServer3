8290bf6ecf3a70fd73bb4d66351d4db79bd95fa8
Mostafa Dorrani <mostafa.dorrani@gmail.com>
2016-08-25 12:17:14 +1000

    User Profile Images

---
+++ source/Core/Models/LastUserNameCookieMessage.cs
@@ -0,0 +1,25 @@
+﻿
+namespace IdentityServer3.Core.Models
+{
+    /// <summary>
+    /// Signed in user data to be written out as cookies.
+    /// </summary>
+    public class LastUserNameCookieMessage : Message
+    {
+        /// <summary>
+        /// Gets or sets the UserName
+        /// </summary>
+        /// <value>
+        /// The UserName
+        /// </value>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Profile picture path
+        /// </summary>
+        /// <value>
+        /// The profile picture path
+        /// </value>
+        public string ProfilePicturePath { get; set; }
+    }
+}
