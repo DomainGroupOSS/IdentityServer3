2ad9e160d31d984b5d01f6fae50e3eced0da8c63
AsifMushtaq <asif.axi@gmail.com>
2018-07-04 11:19:54 +1000

    GS-7678 Added the refresh token check

--- source/Core/Services/Default/DefaultRefreshTokenService.cs
+++ source/Core/Services/Default/DefaultRefreshTokenService.cs
@@ -134,7 +134,10 @@ namespace IdentityServer3.Core.Services.Default
                 var newLifetime = currentLifetime + client.SlidingRefreshTokenLifetime;
                 Logger.Debug("New lifetime: " + newLifetime.ToString());
 
-                if (newLifetime > client.AbsoluteRefreshTokenLifetime)
+                // This is how IdentityServer 4 provides never expiring refresh tokens
+                // zero absolute refresh token lifetime represents unbounded absolute lifetime
+                // if absolute lifetime > 0, cap at absolute lifetime
+                if (client.AbsoluteRefreshTokenLifetime > 0 && newLifetime > client.AbsoluteRefreshTokenLifetime )
                 {
                     newLifetime = client.AbsoluteRefreshTokenLifetime;
                     Logger.Debug("New lifetime exceeds absolute lifetime, capping it to " + newLifetime.ToString());
